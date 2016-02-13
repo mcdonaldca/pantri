a194dad0da428c41513f8845c9dd1f439718ee20
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2016-02-13 09:07:44 -0800

    Let's try more fitbit, shall we.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,21 @@ class MainController < ApplicationController
   end
 
   def fitbit
-    @code = params[:code]
+    auth_encode = Base64.encode64("227GGZ:ee75995a2b98897edce462864c8cf6aa")
+
+    uri = URI('https://api.fitbit.com/oauth2/token')
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+
+    request.add_field('Authorization', 'Basic ' + auth_encode)
+    request.add_field('Content-Type', 'application/x-www-form-urlencoded')
+
+    request.set_form_data({"code" => params[:code], "grant_type" => "authorization_code", "client_id" => "227GGZ"})
+    response = http.request(request)
+    @code = response.body
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'main#index'
+  root to: 'main#index', as: 'index'
 
   # Example of regular route:
   get 'fitbit' => 'main#fitbit'
